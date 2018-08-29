bceaeb20b2b5708b3558c10542bce8ea273e4588
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-29 14:40:15 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,11 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  const listenedBody = document.body
+  listenedBody.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail)
+    if (code.some(element => element === key )) {
+      window.alert("Konami Code")
+    }
+  })
 }
